564721c4bb138e8f310b25b8f97c45a47dc46f9c
osyo_manga <manga.osyo@gmail.com>
2010-12-09 21:04:14 +0900

    has_xxx を追加

---
+++ accelerator/type_traits/detail/has_xxx.hpp
@@ -0,0 +1,18 @@
+// accelerator-detail-library
+// Copyright (c) 2010 osyo-manga : http://d.hatena.ne.jp/osyo-manga/
+//
+
+#ifndef _ACCELERATOR_DETAIL_HAS_XXX_H_
+#define _ACCELERATOR_DETAIL_HAS_XXX_H_
+
+#include <boost/mpl/has_xxx.hpp>
+
+namespace accelerator { namespace detail {
+
+#define ACCELERATOR_DETAIL_HAS_XXX_TRAIT_DEF \
+	BOOST_MPL_HAS_XXX_TRAIT_DEF
+
+} }  // namespace accelerator { namespace detail {
+
+
+#endif  // _ACCELERATORC_DETAIL_HAS_XXX_H_
